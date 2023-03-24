9b9ab3d0418cffe272f341ff4eb18195510e4fce
ndaka99 <ndakaerick@gmail.com>
2023-03-24 11:26:52 -0700

    0x03. C - Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -9,15 +9,4 @@ int i;
 i = 0;
 positive_or_negative(i);
 return (0);
-#include "main.h"
-/**
- * * main - Test function for positive or negative
- * * Return: 0
- * */
-int main(void)
-{
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
-}}
+}
