e0aef3eee5ed95576ef57f6e0b74cfe53e873b58
ndaka99 <ndakaerick@gmail.com>
2023-03-24 14:45:44 -0700

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - Entry point
- * Return: Always return 0 (Success)
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (o);
+	return (0);
 }
